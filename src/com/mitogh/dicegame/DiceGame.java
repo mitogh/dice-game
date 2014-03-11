bb5e483a0220ad2f0ab153a1262c7c69f5892a12
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2014-03-10 20:38:04 -0600

    Initial files for the project

---
+++ src/com/mitogh/dicegame/DiceGame.java
@@ -0,0 +1,23 @@
+package com.mitogh.dicegame;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class DiceGame extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dice_game);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.dice_game, menu);
+        return true;
+    }
+    
+}
