f3b1e75d746d882d340bd236ed97ff1abdb24e42
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2014-03-16 00:10:57 -0600

    Remove useless code and clean some methods

--- src/com/mitogh/dicegame/DiceGame.java
+++ src/com/mitogh/dicegame/DiceGame.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.graphics.Typeface;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
@@ -18,10 +19,8 @@ import android.widget.TextView;
 public class DiceGame extends Activity {
 
 	// GUI Elements
-	private TextView[] mPlayersGUI = {
-			null,
-			null
-	};
+	private TextView mPlayer1Points;
+	private TextView mPlayer2Points;
 	private LinearLayout mPlayerActive;
 	private LinearLayout mPlayerInactive;
 	private TextView mRounds;
@@ -31,8 +30,8 @@ public class DiceGame extends Activity {
 	private TextView mEncouragementMessage;
 	private Button mHold;
 	private Button mRoll;
-	private EditText mPlayer1;
-	private EditText mPlayer2;
+	private EditText mPlayer1Name;
+	private EditText mPlayer2Name;
 	private ImageView mDice;
 	
 	// Internal elements
@@ -47,73 +46,42 @@ public class DiceGame extends Activity {
 	private int round = 1;
 	private Message message;
 	private Sounds mSounds;
+	private AlphaAnimation alphaAnimation;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dice_game);
-        
-        // Search for each ID
-        
+
         // Layouts
         mPlayerActive = (LinearLayout) findViewById(R.id.first);
         mPlayerInactive = (LinearLayout) findViewById(R.id.second);        
         // EditText
-        mPlayer1 = (EditText) findViewById(R.id.editText_player1_name);
-        mPlayer2 = (EditText) findViewById(R.id.editText_player2_name);
+        mPlayer1Name = (EditText) findViewById(R.id.editText_player1_name);
+        mPlayer2Name = (EditText) findViewById(R.id.editText_player2_name);
         // TextView
         mRounds = (TextView) findViewById(R.id.round);
-        mPlayersGUI[0] = (TextView) findViewById(R.id.player1);
-        mPlayersGUI[1] = (TextView) findViewById(R.id.player2);
         mPoints = (TextView) findViewById(R.id.points);
+        
+        mPlayer1Points = (TextView) findViewById(R.id.player1);
+        mPlayer2Points = (TextView) findViewById(R.id.player2);
+        
         mPointsLabelPlayer1 = (TextView) findViewById(R.id.player1_points_label);
         mPointsLabelPlayer2 = (TextView) findViewById(R.id.player2_points_label);
+        
         mEncouragementMessage = (TextView) findViewById(R.id.encouragement_message);
         // Buttons
         mHold = (Button) findViewById(R.id.button_hold);
         mRoll = (Button) findViewById(R.id.button_roll);
-        
+        // Animation
         mDice =  (ImageView) findViewById(R.id.dice); 
-        // Typeface
-        Typeface playball = Typeface.createFromAsset(getAssets(), "fonts/playball.ttf");
-        Typeface roboto_black = Typeface.createFromAsset(getAssets(), "fonts/roboto_black.ttf");
-        Typeface roboto_bold = Typeface.createFromAsset(getAssets(), "fonts/roboto_bold.ttf");
         
-        mRounds.setTypeface(roboto_bold);
-        mPlayer1.setTypeface(playball);
-        mPlayer2.setTypeface(playball);
-        
-        mRounds.setText(getString(R.string.round) + " " + Integer.toString(round));
-        updatePlayersColor();
-        
-        random = new Random();
-        
-        mPlayersGUI[0].setText("0");
-        mPlayersGUI[0].setTypeface(roboto_black);
-        mPointsLabelPlayer1.setTypeface(roboto_black);
-        mPointsLabelPlayer2.setTypeface(roboto_black);
-        
-        players[0].setName(mPlayer1.getText().toString());
-        mPlayersGUI[0].setTextColor(getResources().getColor(R.color.black));
-        mPointsLabelPlayer1.setTextColor(getResources().getColor(R.color.black));
-        
-        mPlayersGUI[1].setText("0");
-        mPlayersGUI[1].setTypeface(roboto_black);
-        mPlayersGUI[1].setTextColor(getResources().getColor(R.color.light_dark));
-        mPointsLabelPlayer2.setTextColor(getResources().getColor(R.color.light_dark));
-        
-        players[0].setName(mPlayer2.getText().toString());
-        mHold.setEnabled(false);
-        mSounds = new Sounds(this);
-        message = new Message(this);
-        final AlphaAnimation alphaAnimation = new AlphaAnimation(0.0f, 1.0f);
-        alphaAnimation.setDuration(1000);
-        
+        setUP();
+
         mRoll.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				
 				// Random number from 1 to 6;
 				number = random.nextInt(6) + 1;
 				message.setNumber(number);
@@ -156,11 +124,46 @@ public class DiceGame extends Activity {
         return true;
     }
     
+    public void setUP(){        
+    	setTypeFace();
+    	updatePlayersColor();
+    	mRounds.setText(getString(R.string.round) + " " + Integer.toString(round));
+        
+        mPlayer1Points.setText("0");
+        mPlayer2Points.setText("0");
+        
+        players[0].setName(mPlayer1Name.getText().toString());
+        players[1].setName(mPlayer2Name.getText().toString());
+        
+        mHold.setEnabled(false);
+        mSounds = new Sounds(this);
+        message = new Message(this);
+        alphaAnimation = new AlphaAnimation(0.0f, 1.0f);
+        alphaAnimation.setDuration(1000);
+        
+        random = new Random();
+    }
+    
+    public void setTypeFace(){
+        Typeface playball = Typeface.createFromAsset(getAssets(), "fonts/playball.ttf");
+        Typeface roboto_black = Typeface.createFromAsset(getAssets(), "fonts/roboto_black.ttf");
+        Typeface roboto_bold = Typeface.createFromAsset(getAssets(), "fonts/roboto_bold.ttf");
+        
+        mRounds.setTypeface(roboto_bold);
+        mPlayer1Name.setTypeface(playball);
+        mPlayer2Name.setTypeface(playball);
+        
+        mPlayer1Points.setTypeface(roboto_black);
+        mPlayer2Points.setTypeface(roboto_black);
+        
+        mPointsLabelPlayer1.setTypeface(roboto_black);
+        mPointsLabelPlayer2.setTypeface(roboto_black);
+    }
+    
     private boolean thereIsWinner(){
     	return players[mCurrentPlayer].isWinner(); 
     }
     
-
     private void gameFlow(){
     	nextPlayer();
     	total = 0;
@@ -210,15 +213,14 @@ public class DiceGame extends Activity {
     }
     
     private void updateScore(){
-    	int number = 0; 
-    	if(mCurrentPlayer == 1){
-    		number = 0; 
+    	if(mCurrentPlayer == 1){ 
+    		players[0].setScore(total);
+    		mPlayer1Points.setText("" + players[0].getScore());
     	}else{
     		number = 1;
+    		players[1].setScore(total);
+    		mPlayer2Points.setText("" + players[1].getScore());
     	}
-    	
-    	players[number].setScore(total);
-    	mPlayersGUI[number].setText("" + players[number].getScore());
     }
     
     private void nextPlayer(){
@@ -229,11 +231,11 @@ public class DiceGame extends Activity {
     
     private void updatePlayersColor(){
     	if(mCurrentPlayer == 1){
-    		mPlayer1.setTextColor(getResources().getColor(R.color.white));
-    		mPlayer2.setTextColor(getResources().getColor(R.color.gray));
+    		mPlayer1Name.setTextColor(getResources().getColor(R.color.white));
+    		mPlayer2Name.setTextColor(getResources().getColor(R.color.gray));
             
-    		mPlayersGUI[0].setTextColor(getResources().getColor(R.color.black));
-    		mPlayersGUI[1].setTextColor(getResources().getColor(R.color.light_dark));
+    		mPlayer1Points.setTextColor(getResources().getColor(R.color.black));
+    		mPlayer2Points.setTextColor(getResources().getColor(R.color.light_dark));
     		
     		mPointsLabelPlayer1.setTextColor(getResources().getColor(R.color.black));
     		mPointsLabelPlayer2.setTextColor(getResources().getColor(R.color.light_dark));
@@ -241,11 +243,11 @@ public class DiceGame extends Activity {
     		mPlayerActive.setBackgroundDrawable(getResources().getDrawable(R.drawable.versus_background_left_active));
             mPlayerInactive.setBackgroundDrawable(getResources().getDrawable(R.drawable.versus_background_right_inactive));
     	}else{
-    		mPlayer2.setTextColor(getResources().getColor(R.color.white));
-    		mPlayer1.setTextColor(getResources().getColor(R.color.gray));
+    		mPlayer2Name.setTextColor(getResources().getColor(R.color.white));
+    		mPlayer1Name.setTextColor(getResources().getColor(R.color.gray));
     		
-    		mPlayersGUI[0].setTextColor(getResources().getColor(R.color.light_dark));
-    		mPlayersGUI[1].setTextColor(getResources().getColor(R.color.black));
+    		mPlayer1Points.setTextColor(getResources().getColor(R.color.light_dark));
+    		mPlayer2Points.setTextColor(getResources().getColor(R.color.black));
     		
     		mPointsLabelPlayer2.setTextColor(getResources().getColor(R.color.black));
     		mPointsLabelPlayer1.setTextColor(getResources().getColor(R.color.light_dark));
